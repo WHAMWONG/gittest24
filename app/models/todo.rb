4c5e860a99392fcd349570d5686234eea1a95a4a
Kat (Jitera) <developers@jitera.com>
2024-01-17 14:57:14 +0000

    feat[service]: Validate todo item details

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,4 @@
+
 class Todo < ApplicationRecord
   has_many :attachments, dependent: :destroy
 
@@ -7,8 +8,18 @@ class Todo < ApplicationRecord
   enum recurrence: %w[daily weekly monthly], _suffix: true
 
   # validations
+  validates :title, presence: true, length: { maximum: 255 }
+
+  validate :validate_due_date
 
   # end for validations
+
+  private
+
+  def validate_due_date
+    errors.add(:due_date, :datetime_in_past) if due_date.present? && due_date < Time.zone.now
+    errors.add(:due_date, :invalid) unless due_date.is_a?(DateTime)
+  end
 
   class << self
   end
